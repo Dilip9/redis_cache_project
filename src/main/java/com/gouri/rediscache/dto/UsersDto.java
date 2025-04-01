72cf03bc3181b4fc8741514b493b30541f5f185b
Dilip9 <dilip.verma940805@gmail.com>
2025-04-01 23:59:20 +0530

    Improvement on some code changes

--- src/main/java/com/gouri/rediscache/dto/UsersDto.java
+++ src/main/java/com/gouri/rediscache/dto/UsersDto.java
@@ -1,4 +1,10 @@
 package com.gouri.rediscache.dto;
 
-public class UsersDto {
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+public record UsersDto(Long id,
+                       @NotNull String username,
+                       @Email @Column(unique = true) String email) {
 }
--- src/main/java/com/gouri/rediscache/entity/Users.java
+++ src/main/java/com/gouri/rediscache/entity/Users.java
@@ -18,6 +18,21 @@ public class Users {
     @Column(unique = true)
     private String email;
 
+    @Column(name="created_at", updatable = false)
     private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
 
 }
